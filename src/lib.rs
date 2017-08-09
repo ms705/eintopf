1b520f15dda616b0344b67d2802bb4f5914c211a
Malte Schwarzkopf <ms705-git@srcf.ucam.org>
2017-08-09 18:01:11 -0400

    First attempt at an RPC-driven eintopf
    Currently doesn't do anything useful (no sane time protocol etc.), but
    does accept connections from client and deserializes RPCs without
    blowing up.

---
+++ src/lib.rs
@@ -0,0 +1,152 @@
+#[macro_use]
+extern crate abomonation;
+extern crate bincode;
+extern crate nix;
+extern crate differential_dataflow;
+extern crate serde;
+#[macro_use]
+extern crate serde_derive;
+extern crate slog;
+extern crate slog_term;
+extern crate timely;
+extern crate timely_communication;
+extern crate rand;
+
+mod types;
+
+use rand::{Rng, SeedableRng, StdRng};
+use std::time;
+use std::net::TcpListener;
+use std::sync::mpsc::{channel, Receiver};
+
+use abomonation::{encode, decode, Abomonation};
+
+use differential_dataflow::collection::AsCollection;
+use differential_dataflow::input::Input;
+use differential_dataflow::operators::*;
+
+use timely::dataflow::operators::{Inspect, Map, Partition};
+use timely::dataflow::operators::capture::{EventReader, Replay};
+use timely::progress::timestamp::RootTimestamp;
+
+use types::Rpc;
+
+pub struct Config {
+    pub articles: usize,
+    pub batch: usize,
+    pub read_mix: usize,
+    pub runtime: Option<u64>,
+    pub workers: usize,
+    pub cluster_cfg: Option<String>,
+    pub pinning: bool,
+}
+
+pub struct Eintopf {
+    config: Config,
+}
+
+#[cfg(target_os = "linux")]
+fn pin_to_core(index: usize, stride: usize) {
+    let mut cpu_set = ::nix::sched::CpuSet::new();
+    let tgt_cpu = index * stride;
+    cpu_set.set(tgt_cpu);
+    let result = ::nix::sched::sched_setaffinity(::nix::unistd::Pid::this(), &cpu_set);
+}
+#[cfg(not(target_os = "linux"))]
+fn pin_to_core(_index: usize, _stride: usize) {
+    println!("core pinning: not linux");
+}
+
+impl Eintopf {
+    pub fn new(c: Config) -> Self {
+        Eintopf { config: c }
+    }
+
+    pub fn run(self) {
+
+        println!("Batching: {:?}", self.config.batch);
+
+        let tc = match self.config.cluster_cfg {
+            None => timely::Configuration::Process(self.config.workers),
+            Some(ref cc) => {
+                timely::Configuration::from_args(
+                    cc.split(" ")
+                        .map(String::from)
+                        .collect::<Vec<_>>()
+                        .into_iter(),
+                ).unwrap()
+            }
+        };
+
+        // set up the dataflow
+        timely::execute(tc, move |worker| {
+
+            let index = worker.index();
+            let peers = worker.peers();
+
+            let listener = TcpListener::bind("0.0.0.0:9999").unwrap();
+
+            // yuck. only accepts one client per worker!
+            let recv = listener.incoming().next().unwrap().unwrap();
+
+            if self.config.pinning {
+                pin_to_core(index, 1);
+            }
+
+            // create a a degree counting differential dataflow
+            let probe = worker.dataflow::<u64, _, _>(|scope| {
+
+                // demultiplex RPCs
+                let streams = EventReader::new(recv).replay_into(scope).partition(4, |x: Rpc| {
+                    match x {
+                        q @ Rpc::Query { .. } => {
+                            println!("got query!");
+                            (0, (q, RootTimestamp::new(0), 1 as isize))
+                        }
+                        i @ Rpc::InsertArticle { .. } => {
+                            println!("got insert article!");
+                            (1, (i, RootTimestamp::new(0), 1 as isize))
+                        }
+                        v @ Rpc::InsertVote { .. } => {
+                            println!("got insert vote!");
+                            (2, (v, RootTimestamp::new(0), 1 as isize))
+                        }
+                        f @ Rpc::Flush => {
+                            println!("got flush!");
+                            (3, (f, RootTimestamp::new(0), 1 as isize))
+                        }
+                    }
+                });
+
+                // unwrap elements in streams and build DD collections to work with; unfortunately
+                // this is a bit unwieldy at the moment.
+                let reads = streams[0].map(|m| match m {
+                    (Rpc::Query { ref view, ref key }, ts, d) => (*key as u64, ts, d),
+                    _ => unreachable!(),
+                }).as_collection();
+                let articles = streams[1].map(|a| match a {
+                    (Rpc::InsertArticle { ref aid, ref title }, ts, d) => ((*aid as u64, title.clone()), ts, d),
+                    _ => unreachable!(),
+                }).as_collection();
+                let votes = streams[2].map(|a| match a {
+                    (Rpc::InsertVote { ref aid, ref uid }, ts, d) => ((*aid, *uid), ts, d),
+                    _ => unreachable!(),
+                }).as_collection();
+                let flushes = streams[3].as_collection();
+
+                // merge votes with articles, to ensure counts for un-voted articles.
+                let votes = votes
+                    .map(|(aid, _uid): (i64, i64)| aid as u64)
+                    .concat(&articles.map(|(aid, _title)| aid as u64));
+
+                // capture artices and votes, restrict by query article ids.
+                let probe = articles.semijoin_u(&votes).semijoin_u(&reads).probe();
+
+                probe
+            });
+
+        }).unwrap();
+
+        println!("Done");
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
 #[macro_use]
 extern crate clap;
-extern crate nix;
+extern crate eintopf;
 extern crate differential_dataflow;
+extern crate nix;
 extern crate slog;
 extern crate slog_term;
 extern crate timely;
@@ -13,6 +14,8 @@ use rand::{Rng, SeedableRng, StdRng};
 
 use differential_dataflow::input::Input;
 use differential_dataflow::operators::*;
+
+use eintopf::{Config, Eintopf};
 
 const NANOS_PER_SEC: u64 = 1_000_000_000;
 macro_rules! dur_to_ns {
@@ -27,72 +30,106 @@ fn main() {
 
     let args = App::new("vote")
         .version("0.1")
-        .about("Benchmarks user-curated news aggregator throughput for in-memory Soup")
-        .arg(Arg::with_name("avg")
-            .long("avg")
-            .takes_value(false)
-            .help("compute average throughput at the end of benchmark"))
-        .arg(Arg::with_name("cdf")
-            .short("c")
-            .long("cdf")
-            .takes_value(false)
-            .help("produce a CDF of recorded latencies for each client at the end"))
-        .arg(Arg::with_name("stage")
-            .short("s")
-            .long("stage")
-            .takes_value(false)
-            .help("stage execution such that all writes are performed before all reads"))
-        .arg(Arg::with_name("distribution")
-            .short("d")
-            .takes_value(true)
-            .default_value("uniform")
-            .help("run benchmark with the given article id distribution [uniform|zipf:exponent]"))
-        .arg(Arg::with_name("ngetters")
-            .short("g")
-            .long("getters")
-            .value_name("N")
-            .default_value("1")
-            .help("Number of GET clients to start"))
-        .arg(Arg::with_name("narticles")
-            .short("a")
-            .long("articles")
-            .value_name("N")
-            .default_value("100000")
-            .help("Number of articles to prepopulate the database with"))
-        .arg(Arg::with_name("runtime")
-            .short("r")
-            .long("runtime")
-            .value_name("N")
-            .default_value("60")
-            .help("Benchmark runtime in seconds"))
-        .arg(Arg::with_name("quiet")
-            .short("q")
-            .long("quiet")
-            .help("No noisy output while running"))
-        .arg(Arg::with_name("core-pin")
-            .long("core-pin")
-            .help("Pin workers to specific cores"))
-        .arg(Arg::with_name("batch_size")
-            .short("b")
-            .long("batch-size")
-            .takes_value(true)
-            .default_value("1000")
-            .help("Input batch size to use."))
-        .arg(Arg::with_name("read_mix")
-            .long("read-mix")
-            .takes_value(true)
-            .default_value("19")
-            .help("Number of reads to perform for each write"))
-        .arg(Arg::with_name("workers")
-            .short("w")
-            .long("workers")
-            .value_name("N")
-            .default_value("1")
-            .help("Number of worker threads"))
-        .arg(Arg::with_name("timely_cluster_cfg")
-            .long("timely-cluster")
-            .takes_value(true)
-            .help("Cluster config string to pass to timely."))
+        .about(
+            "Benchmarks user-curated news aggregator throughput for in-memory Soup",
+        )
+        .arg(
+            Arg::with_name("avg")
+                .long("avg")
+                .takes_value(false)
+                .help("compute average throughput at the end of benchmark"),
+        )
+        .arg(
+            Arg::with_name("cdf")
+                .short("c")
+                .long("cdf")
+                .takes_value(false)
+                .help(
+                    "produce a CDF of recorded latencies for each client at the end",
+                ),
+        )
+        .arg(
+            Arg::with_name("stage")
+                .short("s")
+                .long("stage")
+                .takes_value(false)
+                .help(
+                    "stage execution such that all writes are performed before all reads",
+                ),
+        )
+        .arg(
+            Arg::with_name("distribution")
+                .short("d")
+                .takes_value(true)
+                .default_value("uniform")
+                .help(
+                    "run benchmark with the given article id distribution [uniform|zipf:exponent]",
+                ),
+        )
+        .arg(
+            Arg::with_name("ngetters")
+                .short("g")
+                .long("getters")
+                .value_name("N")
+                .default_value("1")
+                .help("Number of GET clients to start"),
+        )
+        .arg(
+            Arg::with_name("narticles")
+                .short("a")
+                .long("articles")
+                .value_name("N")
+                .default_value("100000")
+                .help("Number of articles to prepopulate the database with"),
+        )
+        .arg(
+            Arg::with_name("runtime")
+                .short("r")
+                .long("runtime")
+                .value_name("N")
+                .default_value("60")
+                .help("Benchmark runtime in seconds"),
+        )
+        .arg(
+            Arg::with_name("quiet")
+                .short("q")
+                .long("quiet")
+                .help("No noisy output while running"),
+        )
+        .arg(
+            Arg::with_name("core-pin")
+                .long("core-pin")
+                .help("Pin workers to specific cores"),
+        )
+        .arg(
+            Arg::with_name("batch_size")
+                .short("b")
+                .long("batch-size")
+                .takes_value(true)
+                .default_value("1000")
+                .help("Input batch size to use."),
+        )
+        .arg(
+            Arg::with_name("read_mix")
+                .long("read-mix")
+                .takes_value(true)
+                .default_value("19")
+                .help("Number of reads to perform for each write"),
+        )
+        .arg(
+            Arg::with_name("workers")
+                .short("w")
+                .long("workers")
+                .value_name("N")
+                .default_value("1")
+                .help("Number of worker threads"),
+        )
+        .arg(
+            Arg::with_name("timely_cluster_cfg")
+                .long("timely-cluster")
+                .takes_value(true)
+                .help("Cluster config string to pass to timely."),
+        )
         .get_matches();
 
     let narticles = value_t_or_exit!(args, "narticles", usize);
@@ -103,148 +140,15 @@ fn main() {
     let cluster_cfg = args.value_of("timely_cluster_cfg");
     let pinning = args.is_present("core-pin");
 
-    run_dataflow(narticles,
-                 bsize,
-                 reads,
-                 runtime,
-                 workers,
-                 cluster_cfg,
-                 pinning);
-}
+    let e = Eintopf::new(Config {
+        articles: narticles,
+        batch: bsize,
+        read_mix: reads,
+        runtime: Some(runtime),
+        workers: workers,
+        cluster_cfg: cluster_cfg.map(|s| String::from(s)),
+        pinning: pinning,
+    });
 
-#[cfg(target_os = "linux")]
-fn pin_to_core(index:usize, stride: usize) {
-    let mut cpu_set = ::nix::sched::CpuSet::new();
-    let tgt_cpu = index * stride;
-    cpu_set.set(tgt_cpu);
-    let result = ::nix::sched::sched_setaffinity(0, &cpu_set);
-}
-#[cfg(not(target_os = "linux"))]
-fn pin_to_core(_index:usize, _stride: usize) {
-    println!("core pinning: not linux");
-}
-
-
-
-fn run_dataflow(articles: usize,
-                batch: usize,
-                read_mix: usize,
-                runtime: u64,
-                workers: usize,
-                cluster_cfg: Option<&str>,
-                pinning: bool) {
-
-    println!("Batching: {:?}", batch);
-
-    let tc = match cluster_cfg {
-        None => timely::Configuration::Process(workers),
-        Some(ref cc) => {
-            timely::Configuration::from_args(cc.split(" ")
-                                                 .map(String::from)
-                                                 .collect::<Vec<_>>()
-                                                 .into_iter())
-                    .unwrap()
-        }
-    };
-
-    // set up the dataflow
-    timely::execute(tc, move |worker| {
-
-        let index = worker.index();
-        let peers = worker.peers();
-
-        if pinning { pin_to_core(index, 1); }
-
-        // create a a degree counting differential dataflow
-        let (mut articles_in, mut votes_in, mut reads_in, probe) = worker.dataflow(|scope| {
-
-            // create input for read request.
-            let (reads_in, reads) = scope.new_collection();
-            let (articles_in, articles) = scope.new_collection();
-            let (votes_in, votes) = scope.new_collection();
-
-            // merge votes with articles, to ensure counts for un-voted articles.
-            let votes = votes.map(|(aid, _uid)| aid)
-                             .concat(&articles.map(|(aid, _title)| aid));
-
-            // capture artices and votes, restrict by query article ids.
-            let probe = articles.semijoin_u(&votes)
-                                .semijoin_u(&reads)
-                                .probe();
-
-            (articles_in, votes_in, reads_in, probe)
-        });
-
-        let seed: &[_] = &[1, 2, 3, index];
-        let mut rng: StdRng = SeedableRng::from_seed(seed);    // rng for edge additions
-
-        let mut writes: Vec<_> = (0 .. articles).collect();
-        rng.shuffle(&mut writes);
-
-        let mut reads: Vec<_> = (0 .. articles).collect();
-        rng.shuffle(&mut reads);
-
-        let timer = time::Instant::now();
-
-        // prepopulate articles
-        if index == 0 {
-            for aid in 0..articles {
-                articles_in.insert((aid, format!("Article #{}", aid)));
-            }
-        }
-
-        // we're done adding articles
-        articles_in.close();
-        votes_in.advance_to(1); votes_in.flush();
-        reads_in.advance_to(1); reads_in.flush();
-        worker.step_while(|| probe.less_than(votes_in.time()));
-
-        if index == 0 {
-            println!("Loading finished after {:?}", timer.elapsed());
-        }
-
-        // now run the throughput measurement
-        let start = time::Instant::now();
-        let mut round = 1;
-
-        while start.elapsed() < time::Duration::from_millis(runtime * 1000) {
-
-            for count in 0 .. batch {
-
-                let local_step = round * batch + count;
-                let logical_time = local_step * peers + index;
-
-                // either write a vote, or read an article.
-                if local_step % (read_mix + 1) == 0 {
-                    votes_in.advance_to(logical_time);
-                    votes_in.insert((writes[local_step % writes.len()], 0))
-                } 
-                else {
-                    reads_in.advance_to(logical_time);
-                    reads_in.insert(reads[local_step % writes.len()]);
-                    reads_in.advance_to(logical_time + 1);
-                    reads_in.remove(reads[local_step % writes.len()]);
-                }   
-
-            }
-
-            round += 1;
-
-            votes_in.advance_to(round * batch * peers); votes_in.flush();
-            reads_in.advance_to(round * batch * peers); reads_in.flush();
-            worker.step_while(|| probe.less_than(votes_in.time()));
-        }
-
-        // remove the first round, in which we loaded the data.
-        round -= 1;
-
-        if index == 0 {
-            println!("processed {} events in {}s => {}",
-                     round * batch * workers,
-                     dur_to_ns!(start.elapsed()) as f64 / NANOS_PER_SEC as f64,
-                     (round * batch * workers) as f64 / (dur_to_ns!(start.elapsed()) / NANOS_PER_SEC as f64));
-        }
-    }).unwrap();
-
-    println!("Done");
+    e.run()
 }
